19bf9a4bd03c4625c7ba521dd977260665805064
Alve Elde <alve_elde@hotmail.com>
2019-11-06 13:18:54 +0100

    Add mapreducejob

--- map-reduce/__main__.py
+++ map-reduce/__main__.py
@@ -1,17 +1,9 @@
-from .corpus import InMemoryDocument, InMemoryCorpus
-from .invertedindex import InMemoryInvertedIndex
-from .normalization import BrainDeadNormalizer
-from .tokenization import BrainDeadTokenizer
+from .mapreducejob import MapReduceJob
 
-
-
-def main():
-    corpus = InMemoryCorpus()
-    normalizer = BrainDeadNormalizer()
-    tokenizer = BrainDeadTokenizer()
-    corpus.add_document(InMemoryDocument(0, {"body": "this is a Test"}))
-    corpus.add_document(InMemoryDocument(1, {"body": "test TEST prØve"}))
-    index = InMemoryInvertedIndex(corpus, ["body"], normalizer, tokenizer)
+def run_mapreducejob():
+    mapreducejob = MapReduceJob()
 
 if __name__ == "__main__":
-    main()
+    #TODO: Parse arguments
+    #TODO: Choose program to run
+    run_mapreducejob()
